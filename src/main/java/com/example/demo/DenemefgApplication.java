d4cdbbc0043e9fe171c38d6257bcc913882da055
fatmag8 <fatmagezerr1997@gmail.com>
2020-10-04 12:41:16 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/DenemefgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenemefgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DenemefgApplication.class, args);
+    }
+
+}
